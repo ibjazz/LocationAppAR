83b5de19d9cff24ad35680c1ed0a6c8adb93e099
Ignacio Bermeo <ibermeo@me.com>
2012-03-28 18:02:41 -0600

    Initial Commit

---
+++ LocationAppAR/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  LocationAppAR
+//
+//  Created by Ignacio Bermeo Juarez on 3/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
